e7a051d24fa05c000f6599ca1a7cf5e58dadb997
Wassilette <wassila.lachgar@gmail.com>
2020-02-07 17:22:18 +0100

    all files added

---
+++ app.js
@@ -0,0 +1,89 @@
+let userScore = 0; //this is called cashing the DOM
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice(){
+  const choices = ['r', 'p', 's'];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+
+function convertToWord(letter){
+  if (letter === "r") {
+     return "Rock";
+  }
+  else if (letter === "p"){
+    return "Paper";
+  }
+  else{
+    return "Scissors";
+  }
+}
+
+function win(userChoice, computerChoice){
+  const smallUserWord = " user".fontsize(3).sub();
+  const smallCompWord = " comp".fontsize(3).sub();
+  const userChoice_div = document.getElementById(userChoice);
+  userScore ++;document.getElementById('r')
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)} ${(smallUserWord)} beats ${convertToWord(computerChoice)} ${(smallCompWord)} . You win !`; //other way to concatenate in js6
+  userChoice_div.classList.add('green-glow'); //add a class
+  setTimeout(() => userChoice_div.classList.remove('green-glow'), 300); //remove a class (after a certain time)
+}
+
+function lose(userChoice, computerChoice){
+  const smallUserWord = " user".fontsize(3).sub();
+  const smallCompWord = " comp".fontsize(3).sub();
+  const userChoice_div = document.getElementById(userChoice);
+  computerScore ++;
+  userScore_span.innerHTML = userScore;
+  computerScore_span.innerHTML = computerScore;
+  result_p.innerHTML = `${convertToWord(userChoice)} ${(smallUserWord)} loses to ${convertToWord(computerChoice)} ${(smallCompWord)} . You lost !`;
+  userChoice_div.classList.add('red-glow');
+  setTimeout(() => userChoice_div.classList.remove('red-glow'), 300);
+}
+
+function draw(userChoice, computerChoice){
+  const smallUserWord = " user".fontsize(3).sub();
+  const smallCompWord = " comp".fontsize(3).sub();
+  const userChoice_div = document.getElementById(userChoice);
+  result_p.innerHTML = `${convertToWord(userChoice)} ${(smallUserWord)} equals ${convertToWord(computerChoice)} ${(smallCompWord)} . It's a draw !`;
+  userChoice_div.classList.add('gray-glow');
+  setTimeout(() => userChoice_div.classList.remove('gray-glow'), 300);
+}
+
+function game(userChoice){
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, computerChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, computerChoice);
+      break;
+    default:
+      draw(userChoice, computerChoice);
+  }
+}
+
+function main(){
+  rock_div.addEventListener('click', () => game("r"));
+
+  paper_div.addEventListener('click', () => game("p"));
+
+  scissors_div.addEventListener('click', () =>
+  game("s"));
+}
+
+main();
